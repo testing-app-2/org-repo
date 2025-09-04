ca23bdec52f807ea37f54a3e38a6f31411530932
Shreyansh-Chachaundiya <textcodecopy@gmail.com>
2025-09-04 11:01:31 +0530

    Add files via upload

--- auth_routes.py
+++ auth_routes.py
@@ -1,5 +1,5 @@
 import uuid
-from fastapi import APIRouter, Form, HTTPException, Response, Depends
+from fastapi import APIRouter, Form, HTTPException, Response, Depends, BackgroundTasks
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.config import COOKIE_EXPIRE_SECONDS, COOKIE_DOMAIN, COOKIE_SAMESITE
@@ -8,6 +8,7 @@ from app.database import get_db
 from app.crud.users import insert_user, get_user
 from app.crud.codes import get_code, update_code
 from app.services.generate_code import generate_code
+from app.services.otp_util import generate_and_send_code
 from app.services.send_code import send_code
 from app.services.auth_utils.create_token import create_token
 from app.services.validate_email import is_valid_email
@@ -17,7 +18,13 @@ router = APIRouter()
 
 
 @router.post("/auth/signup")
-async def signup_handler(name: str = Form(), email: str = Form(), organization: str = Form(default=None), db_session: AsyncSession = Depends(get_db)):
+async def signup_handler(
+    background_tasks: BackgroundTasks,
+    name: str = Form(),
+    email: str = Form(),
+    organization: str = Form(default=None),
+    db_session: AsyncSession = Depends(get_db),
+):
     email = email.lower()
     if not await is_valid_email(email):
         logger.warning("Invalid email provided.", extra={"email": email})
@@ -30,27 +37,37 @@ async def signup_handler(name: str = Form(), email: str = Form(), organization: 
             status_code=409, detail="Email already exists."
         )  # Raise an exception if email exists
 
-    code = await generate_code(db_session, email, "auth")
-    await send_code(email, code, "verification")
+    background_tasks.add_task(generate_and_send_code, email, db_session)
     # await publish_event(event_type="User.OTPGenerated", subject=f"user/{email}", data={"email": email, "otp": code, "action": "signup"})
 
     logger.info("Verification code sent.", extra={"email": email})
-    return {"status_code": 200, "message": "Code sent", "email": email, "name": name, "organization": organization}
+    return {
+        "status_code": 200,
+        "message": "Code sent",
+        "email": email,
+        "name": name,
+        "organization": organization,
+    }
 
 
 @router.post("/auth/signup/validate")
 async def signup_validate_handler(
-    response: Response, name: str = Form(), email: str = Form(), organization: str = Form(default=None), code: str = Form(), db_session: AsyncSession = Depends(get_db)
+    response: Response,
+    name: str = Form(),
+    email: str = Form(),
+    organization: str = Form(default=None),
+    code: str = Form(),
+    db_session: AsyncSession = Depends(get_db),
 ):
     email = email.lower()
     if not await is_valid_email(email):
         logger.warning("Invalid email provided.", extra={"email": email})
         raise HTTPException(status_code=403, detail="Invalid email.")
-    
-    if len(code) != 4:
+
+    if len(code) != 6:
         logger.warning("Invalid code provided.", extra={"email": email, "code": code})
         raise HTTPException(status_code=403, detail="Invalid code.")
-    
+
     result = await get_code(db_session, email, code)
     if result is None:
         logger.warning("Invalid code provided.", extra={"email": email})
@@ -71,16 +88,24 @@ async def signup_validate_handler(
         max_age=COOKIE_EXPIRE_SECONDS,
         samesite=COOKIE_SAMESITE,
         secure=True,
-        domain=COOKIE_DOMAIN
+        domain=COOKIE_DOMAIN,
     )
     user = await get_user(db_session, email)
-    await publish_event(event_type="User.SignUp", subject=f"signup/{email}", data={"email": email, "name": name})
+    await publish_event(
+        event_type="User.SignUp",
+        subject=f"signup/{email}",
+        data={"email": email, "name": name},
+    )
     logger.info("User created and logged in", extra={"email": email})
     return user
 
 
 @router.post("/auth/login")
-async def login_handler(email: str = Form(), db_session: AsyncSession = Depends(get_db)):
+async def login_handler(
+    background_tasks: BackgroundTasks,
+    email: str = Form(),
+    db_session: AsyncSession = Depends(get_db),
+):
     email = email.lower()
     if not await is_valid_email(email):
         logger.warning("Invalid email provided.", extra={"email": email})
@@ -89,10 +114,11 @@ async def login_handler(email: str = Form(), db_session: AsyncSession = Depends(
     user = await get_user(db_session, email)
     if user is None:
         logger.warning("Login attempt with non-existing email", extra={"email": email})
-        raise HTTPException(status_code=400, detail="Email does not exist. Please sign up.")
+        raise HTTPException(
+            status_code=400, detail="Email does not exist. Please sign up."
+        )
 
-    code = await generate_code(db_session, email, "auth")
-    await send_code(email, code, "auth")
+    background_tasks.add_task(generate_and_send_code, email, db_session)
     # await publish_event(event_type="User.OTPGenerated", subject=f"user/{email}", data={"email": email, "otp": code, "action": "login"})
 
     logger.info("Authentication code sent", extra={"email": email})
@@ -100,16 +126,21 @@ async def login_handler(email: str = Form(), db_session: AsyncSession = Depends(
 
 
 @router.post("/auth/login/validate")
-async def login_validate_handler(response: Response, email: str = Form(), code: str = Form(), db_session: AsyncSession = Depends(get_db)):
+async def login_validate_handler(
+    response: Response,
+    email: str = Form(),
+    code: str = Form(),
+    db_session: AsyncSession = Depends(get_db),
+):
     email = email.lower()
     if not await is_valid_email(email):
         logger.warning("Invalid email provided.", extra={"email": email})
         raise HTTPException(status_code=403, detail="Invalid email.")
-    
-    if len(code) != 4:
+
+    if len(code) != 6:
         logger.warning("Invalid code provided.", extra={"email": email, "code": code})
         raise HTTPException(status_code=403, detail="Invalid code.")
-    
+
     result = await get_code(db_session, email, code)
     if result is None:
         logger.warning("Invalid code provided.", extra={"email": email})
@@ -126,7 +157,7 @@ async def login_validate_handler(response: Response, email: str = Form(), code: 
         max_age=COOKIE_EXPIRE_SECONDS,
         samesite=COOKIE_SAMESITE,
         secure=True,
-        domain=COOKIE_DOMAIN
+        domain=COOKIE_DOMAIN,
     )
     logger.info("User logged in", extra={"email": email})
     user = await get_user(db_session, email)
@@ -142,7 +173,7 @@ async def logout_handler(response: Response):
         max_age=0,
         samesite=COOKIE_SAMESITE,
         secure=True,
-        domain=COOKIE_DOMAIN
+        domain=COOKIE_DOMAIN,
     )
     logger.info("User logged out")
     return {"message": "Logged out"}
